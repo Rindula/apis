ffc5b065a23e16968e33a460fd40f3841140d11d
Rindula <contact@rindula.de>
2019-04-07 01:35:40 +0200

    Update instagram.php

--- bigdata/instagram.php
+++ bigdata/instagram.php
@@ -2,9 +2,23 @@
 
 $cred = $_POST["cred"];
 
-if ($cred != "H@254NabW^Z!w7jc;$Ez\"=tr`\RnJKk#CS~-sD,%AV'eU.9yXfhQuG/dvTq&*Mp6") die();
+if ($cred != "3l7qC9Nn2hUJ2glA0ykTNlt649nOg28QvqaotkJkfuqIjVwJPO5NMibOWujljuvO") die();
 
 $id = $_POST["uname"];
 $name = $_POST["name"];
 $follower = $_POST["follower"];
 $follows = $_POST["follows"];
+
+require "../../secrets.php";
+
+$dbh = new PDO('mysql:host=localhost;dbname=bigdata', DB_USER, DB_PASSWORD);
+$dbh->query('SET NAMES utf8');
+
+$stmt = $dbh->prepare("INSERT INTO instagram (id, name, follower, follows) VALUES (:id, :name, :follower, :follows)");
+
+$stmt->bindParam(":id", $id);
+$stmt->bindParam(":name", $name);
+$stmt->bindParam(":follower", $follower);
+$stmt->bindParam(":follows", $follows);
+
+$stmt->execute();
