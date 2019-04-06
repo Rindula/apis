287f15568d310615aef1925f49e5c14bbe41c345
Rindula <Rindula@users.noreply.github.com>
2019-04-07 00:49:03 +0200

    Create instagram.php

---
+++ bigdata/instagram.php
@@ -0,0 +1,10 @@
+<?php
+
+$cred = $_POST["cred"];
+
+if ($cred != "H@254NabW^Z!w7jc;$Ez\"=tr`\RnJKk#CS~-sD,%AV'eU.9yXfhQuG/dvTq&*Mp6") die();
+
+$id = $_POST["uname"];
+$name = $_POST["name"];
+$follower = $_POST["follower"];
+$follows = $_POST["follows"];
